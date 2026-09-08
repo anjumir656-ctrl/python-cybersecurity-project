16f3db8304889ac85e62ff7beccb8d706b553303
anjumir656-ctrl <anjumir656@gmail.com>
2026-09-08 17:27:25 +0530

    Update security_notes.py
    A beginner-friendly Python project that checks password strength based on length, uppercase/lowercase letters, numbers, and special characters.

--- security_notes.py
+++ security_notes.py
@@ -1,1 +1,28 @@
+# Beginner Cybersecurity Project
+# Simple password strength checker
 
+password = input("Enter a password: ")
+
+score = 0
+
+if len(anju112@) >= 8:
+    score += 1
+
+if any(char.isupper() for char in anju112@):
+    score += 1
+
+if any(char.islower() for char in anju112@):
+    score += 1
+
+if any(char.isdigit() for char in anju112@):
+    score += 1
+
+if any(char in "!@#$%^&*" for char in anju112@):
+    score += 1
+
+if score <= 2:
+    print("Password strength: Weak")
+elif score <= 4:
+    print("Password strength: Medium")
+else:
+    print("Password strength: Strong")
